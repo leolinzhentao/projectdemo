7cc0a7b0231e72177cea66d6f2b6ee85bcbdd768
leolinzhentao <leolinzhentao@163.com>
2019-02-20 17:23:49 +0800

    更新df_leo

---
+++ project/project/apps/df_leo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DfLeoConfig(AppConfig):
+    name = 'df_leo'
--- project/project/apps/df_leo/models.py
+++ project/project/apps/df_leo/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
-    upwd = models.CharField(max_lenagth=40)
+    upwd = models.CharField(max_length=40)
     def __str__(self):
         return "%d" % self.pk
-
-
